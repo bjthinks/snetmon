bce5fcf79b01ecd2b9434deb8b84fcf6653478ce
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2014-04-14 12:17:55 -0700

    Add Device::query(str), which forks off an snmpbulkwalk request, returning the response via a Pipe

--- device.cc
+++ device.cc
@@ -1,5 +1,6 @@
 #include <string>
 
+#include "util.hh"
 #include "device.hh"
 
 using namespace std;
@@ -8,3 +9,9 @@ Device::Device(const std::string &h, const std::string &c)
   : hostname(h),
     community(c)
 {}
+
+unique_ptr<Pipe> Device::query(const string &q)
+{
+  return unique_ptr<Pipe>(new Pipe(string("snmpbulkwalk -Oq -v2c -c")
+                                   + community + ' ' + hostname + ' ' + q));
+}
--- device.hh
+++ device.hh
@@ -1,12 +1,14 @@
 #ifndef DEVICE_HH
 #define DEVICE_HH
 
+#include <memory>
 #include <string>
 
 class Device
 {
 public:
   Device(const std::string &h, const std::string &c);
+  std::unique_ptr<Pipe> query(const std::string &q);
 
 private:
   std::string hostname;
