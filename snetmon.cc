196ddf6ea841b0faa116cf872984ccb410494934
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2014-04-14 12:18:51 -0700

    Query number of interfaces as a demo

--- snetmon.cc
+++ snetmon.cc
@@ -1,6 +1,7 @@
 #include <vector>
 #include <iostream>
 
+#include "util.hh"
 #include "device.hh"
 #include "config.hh"
 
@@ -10,6 +11,13 @@ int main()
 {
   vector<Device> devices = read_config_file();
   cout << "Read " << devices.size() << " devices\n";
+
+  for (int i = 0; i < int(devices.size()); ++i) {
+    unique_ptr<Pipe> p = devices[i].query("IF-MIB::ifNumber.0");
+    string line;
+    while (getline(p->stream(), line))
+      cout << line << '\n';
+  }
 
   return 0;
 }
