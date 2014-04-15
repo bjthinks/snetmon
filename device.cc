855d658055d3d5328734213533e36b7951aab8bf
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2014-04-15 12:28:33 -0700

    Fix typo and rename variables for clarity in Device::setOnInterfaces()

--- device.cc
+++ device.cc
@@ -22,14 +22,16 @@ void Device::setOnInterfaces(const string &q,
     // Assume the response is of the following form:
     // "Blahblah.<interface number> <value>"
     //          ^                  ^        parse it using these delimiters
-    pair<string, string> line_words = split(line);
-    vector<string> first_word_parts = words(line_words.first, ".");
+    pair<string, string> line_parts = split(line);
+    string first_word = line_parts.first;
+    string rest_of_line = line_parts.second;
+    vector<string> first_word_parts = words(first_word, ".");
 
     // If there was more than one '.' in the first word, ignore
     // the line. Otherwise, set a value on the interface number.
     if (first_word_parts.size() == 2) {
-      int interface = stoi(first_part_words[1]);
-      interfaces.at(interface).setGeneric(line_parts.second, setter);
+      int interface = stoi(first_word_parts[1]);
+      interfaces.at(interface).setGeneric(rest_of_line, setter);
     }
   }
 }
