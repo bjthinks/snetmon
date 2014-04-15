cfa34f1a8defed6d880fa1d29d830927ec65aa7c
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2014-04-15 03:44:20 -0700

    Demo program prints a list of interface IDs

--- snetmon.cc
+++ snetmon.cc
@@ -10,13 +10,25 @@ using namespace std;
 int main()
 {
   vector<Device> devices = read_config_file();
-  cout << "Read " << devices.size() << " devices\n";
 
-  for (int i = 0; i < int(devices.size()); ++i) {
-    unique_ptr<Pipe> p = devices[i].query("IF-MIB::ifNumber.0");
+  for (int d = 0; d < int(devices.size()); ++d) {
+    Device &device = devices[d];
+
+    unique_ptr<Pipe> p = device.query("IF-MIB::ifIndex");
     string line;
-    while (getline(p->stream(), line))
-      cout << line << '\n';
+    while (getline(p->stream(), line)) {
+      vector<string> line_words = words(line);
+      if (line_words.size() == 2) {
+        int i = stoi(line_words[1]);
+        device.addInterface(i);
+      }
+    }
+
+    Device::iterator i;
+    for (i = device.begin(); i != device.end(); ++i) {
+      int interface_number = i->first;
+      cout << interface_number << '\n';
+    }
   }
 
   return 0;
