553fce264f56c13694f32e02b73a602267f5c0c6
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2014-04-15 12:03:08 -0700

    Add getters/setters for Interface::bytesIn and ::bytesOut

--- interface.hh
+++ interface.hh
@@ -6,17 +6,26 @@
 class Interface
 {
 public:
+  // Setters
   void setName(const std::string &s) { name_ = s; }
   void setAlias(const std::string &s) { alias_ = s; }
   void setDescription(const std::string &s) { description_ = s; }
+  void setBytesIn(const int64_t &b) { bytesIn_ = b; }
+  void setBytesOut(const int64_t &b) { bytesOut_ = b; }
+
+  // Corresponding getters
   const std::string &name() const { return name_; }
   const std::string &alias() const { return alias_; }
   const std::string &description() const { return description_; }
+  const int64_t bytesIn() const { return bytesIn_; }
+  const int64_t bytesOut() const { return bytesOut_; }
 
 private:
   std::string name_;
   std::string alias_;
   std::string description_;
+  int64_t bytesIn_;
+  int64_t bytesOut_;
 };
 
 #endif
